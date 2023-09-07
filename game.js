12551515b7070584dc3327414816b9474fde18b0
doomguyslays <122403136+doomguyslays@users.noreply.github.com>
2023-09-07 13:22:20 +0000

    Update game.js

--- game.js
+++ game.js
@@ -1,118 +1,92 @@
-const canvas = document.getElementById('gameCanvas');
-const ctx = canvas.getContext('2d');
+const ball = document.querySelector('.ball');
+const scoreDisplay = document.getElementById('score');
+const highscoreDisplay = document.getElementById('highscore');
+const coinDisplay = document.getElementById('ball-coins');
+const buyBallButton = document.getElementById('buy-ball-button');
+const BALL_COST = 50;
 
-// Function to resize canvas
-function resizeCanvas() {
-  canvas.width = canvas.parentElement.offsetWidth;
-  canvas.height = canvas.parentElement.offsetHeight;
+let score = 0;
+let highscore = 0;
+let ballCoins = 0;
+
+function updateCoinDisplay() {
+    coinDisplay.textContent = ballCoins;
 }
 
-// Call resizeCanvas function initially and on window resize
-resizeCanvas();
-window.addEventListener('resize', resizeCanvas);
-
-let ballpoints = 0;
-let ballCost = 10;
-
-let balls = [
-  {
-    x: canvas.width / 2,
-    y: canvas.height / 2,
-    radius: 10,
-    dx: 2,
-    dy: -2,
-    color: 'red'
-  }
-];
-
-function drawBall(ball) {
-  ctx.beginPath();
-  ctx.arc(ball.x, ball.y, ball.radius, 0, Math.PI * 2);
-  ctx.fillStyle = ball.color;
-  ctx.fill();
-  ctx.closePath();
+function earnBallCoins(amount) {
+    ballCoins += amount;
+    updateCoinDisplay();
 }
 
-function updateBall(ball) {
-  if (ball.x + ball.dx > canvas.width - ball.radius || ball.x + ball.dx < ball.radius) {
-    ball.dx = -ball.dx;
-    ballpoints++;
-  }
+buyBallButton.addEventListener('click', () => {
+    if (ballCoins >= BALL_COST) {
+        ballCoins -= BALL_COST;
+        updateCoinDisplay();
+        createNewBall();
+    } else {
+        alert('Not enough Ball Coins to buy a ball.');
+    }
+});
 
-  if (ball.y + ball.dy > canvas.height - ball.radius || ball.y + ball.dy < ball.radius) {
-    ball.dy = -ball.dy;
-    ballpoints++;
-  }
+function createNewBall() {
+    const newBall = document.createElement('div');
+    newBall.classList.add('ball');
+    document.querySelector('.game-container').appendChild(newBall);
 
-  ball.x += ball.dx;
-  ball.y += ball.dy;
+    const newX = Math.random() * (window.innerWidth - 50);
+    const newY = Math.random() * (window.innerHeight - 50);
+    const newDx = (Math.random() - 0.5) * 10;
+    const newDy = (Math.random() - 0.5) * 10;
+
+    newBall.style.left = newX + 'px';
+    newBall.style.top = newY + 'px';
+
+    let count = 0;
+
+    function updateBallPosition() {
+        newX += newDx;
+        newY += newDy;
+
+        if (newX + newBall.clientWidth > window.innerWidth || newX < 0) {
+            newDx = -newDx;
+        }
+
+        if (newY + newBall.clientHeight > window.innerHeight || newY < 0) {
+            newDy = -newDy;
+        }
+
+        newBall.style.left = newX + 'px';
+        newBall.style.top = newY + 'px';
+
+        if (count < 1000) {
+            count++;
+            requestAnimationFrame(updateBallPosition);
+        } else {
+            newBall.remove();
+        }
+    }
+
+    updateBallPosition();
 }
 
-function drawPoints() {
-  ctx.font = "16px Arial";
-  ctx.fillStyle = "#0095DD";
-  ctx.fillText("Ballpoints: "+ballpoints, 8, 20);
+function updateScore() {
+    score++;
+    scoreDisplay.textContent = score;
+    earnBallCoins(1);
+
+    if (score > highscore) {
+        highscore = score;
+        highscoreDisplay.textContent = highscore;
+    }
+}
+
+function updateBallPosition() {
+    // Existing ball movement code
 }
 
 function gameLoop() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-  balls.forEach((ball) => {
-    drawBall(ball);
-    updateBall(ball);
-  });
-
-  drawPoints();
-
-  requestAnimationFrame(gameLoop);
+    updateBallPosition();
+    requestAnimationFrame(gameLoop);
 }
-
-function buyBall() {
-  if (ballpoints >= ballCost) {
-    ballpoints -= ballCost;
-    ballCost *= 2;
-    balls.push({
-      x: Math.random() * canvas.width,
-      y: Math.random() * canvas.height,
-      radius: 10,
-      dx: (Math.random() - 0.5) * 4,
-      dy: (Math.random() - 0.5) * 4,
-      color: 'red'
-    });
-  } else {
-    alert('Not enough ballpoints!');
-  }
-}
-
-canvas.addEventListener('touchmove', (event) => {
-  const rect = canvas.getBoundingClientRect();
-  const scaleX = canvas.width / rect.width;  
-  const scaleY = canvas.height / rect.height;
-  balls[0].x = (event.touches[0].clientX - rect.left) * scaleX;
-  balls[0].y = (event.touches[0].clientY - rect.top) * scaleY;
-});
-
-document.getElementById('buyBallButton').addEventListener('click', buyBall);
-
-// Load the saved game state from localStorage when the page loads
-window.onload = () => {
-  const savedState = localStorage.getItem('gameState');
-  if (savedState) {
-    const { savedPoints, savedCost, savedBalls } = JSON.parse(savedState);
-    ballpoints = savedPoints;
-    ballCost = savedCost;
-    balls = savedBalls;
-  }
-};
-
-// Save the game state to localStorage every second
-setInterval(() => {
-  const gameState = {
-    savedPoints: ballpoints,
-    savedCost: ballCost,
-    savedBalls: balls
-  };
-  localStorage.setItem('gameState', JSON.stringify(gameState));
-}, 1000);
 
 gameLoop();
